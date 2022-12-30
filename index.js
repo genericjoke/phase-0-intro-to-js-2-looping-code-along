c8b241d2e4cba8fff1377f32667a44dbb807d0ec
Nick Sollon <genericjoke@gmail.com>
2022-12-30 15:38:52 -0500

    commit tree 10d8a90f5afc916d7448ce45e24b974493879a9e

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(obj, event) {
+    const thanks = [];
+    for (let x = 0; x < obj.length; x++)
+        thanks.push("Thank you, " + obj[x] + ", for the wonderful " + event + " gift!");
+        return thanks;
+}
+function countDown(pos){
+    while(pos>=0){
+        console.log(pos);
+        pos--;
+    }
+}
